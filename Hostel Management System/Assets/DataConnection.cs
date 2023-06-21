d93b8371f83f10a7ff50e9bb6d7f8b5985b4e80d
DiyRex <dsdissanayaka2002@gmail.com>
2023-06-21 20:53:43 +0530

    [+] Database Added [+] Connections Established

--- Hostel Management System/Assets/DataConnection.cs
+++ Hostel Management System/Assets/DataConnection.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Hostel_Management_System.Assets
 {
     internal class DataConnection
     {
+        private SqlConnection connection;
+        private string connectionString;
+
+        public DataConnection()
+        {
+            // Set your connection string here
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=.\Database\HostelDB.mdf;Integrated Security=True";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionString);
+            }
+
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+
+        public void CloseConnection()
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
     }
 }
